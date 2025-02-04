f6bd86f13fd3f070396b9278ca89a0292efa01d7
wayne-bai-ics4u1-02-25 <wayne.bai@student.tdsb.on.ca>
2025-02-04 15:19:32 +0000

    I finish task, I go home sir

--- src/main/java/Review1.java
+++ src/main/java/Review1.java
@@ -1,23 +1,45 @@
 public class Review1 {
 
 	public static void q1() {
-		//Write question 1 code here
+		System.out.println("There once was a man from St. Ives.");
 	}
 
 	public static void q2() {
-		//Write question 2 code here
+		int num1 = 9;
+		int num2 = 5;
+		System.out.println(num1/num2);
+		System.out.println(num1 % num2);
 	}
 
 	public static void q3() {
-		//Write question 3 code here
+		System.out.println("Hello\nHello again");
 	}
 
 	public static void q4() {
-		//Write question 4 code here
+		System.out.println(true);
 	}
 
 	public static void q5() {
-		//Write question 5 code here
+		String greet = "Hello";
+		String subject = "World";
+
+		System.out.print(greet);
+		System.out.println(" " + subject);
 	}
 
 }
+/*
+[![Open in Codespaces](https://classroom.github.com/assets/launch-codespace-2972f46106e565e64193e422d61a12cf1da4916b45550586e14ef0a7c637dd04.svg)](https://classroom.github.com/open-in-codespaces?assignment_repo_id=18022292)
+# Instructions  
+
+  ## Questions
+  1. Output the sentence “There once was a man from St. Ives.”
+  2. Create two integer variables with the values 9 and 5.  Output the quotient (rounded down) and the remainder on separate lines.
+  3. Write code that will produce the following printout using only a single println().
+~~~
+Hello
+Hello again
+~~~
+4. Output the boolean true.
+5. Create two Strings. The first should have the value "Hello", the second should have the value "World".  Output Hello World on the same line using two print statements with a space between them.
+*/
